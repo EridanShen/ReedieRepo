eba09990501c9a384fcf9ed96c99f8f5591a93b7
EridanShen <eridan1310@gmail.com>
2024-06-16 22:59:24 +0700

    Первая тема

--- First Theme/Task 5.py
+++ First Theme/Task 5.py
@@ -1,4 +1,3 @@
-# Создаем словарь с данными о человеке
 person = {
     "имя": "Джонни",
     "возраст": 27,
@@ -7,18 +6,15 @@ person = {
     "вес": 60
 }
 
-# Выводим на экран все данные о человеке по ключам
 print("Данные о человеке:")
 for key, value in person.items():
     print(f"{key.capitalize()}: {value}")
 
-# Добавляем в словарь ключ и значение размера ноги
 person["размер ноги"] = 43
 print("\nПосле добавления размера ноги:")
 for key, value in person.items():
     print(f"{key.capitalize()}: {value}")
 
-# Удаляем из словаря возраст
 del person["возраст"]
 print("\nПосле удаления возраста:")
 for key, value in person.items():
