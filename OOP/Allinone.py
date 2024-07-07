1db65c543d0fc0548e2a9b0e552109a834d67c30
EridanShen <eridan1310@gmail.com>
2024-07-07 15:21:45 +0700

    OOP All in One Task

--- OOP/Allinone.py
+++ OOP/Allinone.py
@@ -114,7 +114,7 @@ class Dog:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Dog, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Dog, cls).__new__(cls)
         return cls._instance
 
     def __init__(self, name, age):
